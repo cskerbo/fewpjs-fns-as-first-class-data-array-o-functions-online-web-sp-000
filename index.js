3379aae79f2ba6cae9580d36157fcaed19875390
Chris Kerbo <chris.kerbo@outlook.com>
2020-03-17 00:16:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 function wakeDog(dogName, dogBreed) {
-  console.log(`Wake ${dogName} the ${dogBreed}`);
+  return `Wake ${dogName} the ${dogBreed}`
 }
 
 function leashDog(dogName, dogBreed) {
-  console.log(`Leash ${dogName} the ${dogBreed}`);
+  return `Leash ${dogName} the ${dogBreed}`
 }
 
 function walkToPark(dogName, dogBreed) {
-  console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
+  return `Walk to the park with ${dogName} the ${dogBreed}`
 }
 
 function throwFrisbee(dogName, dogBreed) {
-  console.log(`Throw the frisbee with ${dogName} the ${dogBreed}`);
+  return `Throw the frisbee with ${dogName} the ${dogBreed}`
 }
 
 function walkHome(dogName, dogBreed) {
-  console.log(`Walk home with ${dogName} the ${dogBreed}`);
+  return `Walk home with ${dogName} the ${dogBreed}`
 }
 
 function unleashDog(dogName, dogBreed) {
